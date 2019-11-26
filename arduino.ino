6c24ff6b03e8ea28ac3c9b414c1e23deddccdb3a
Roland <roland.debnarik@gmail.com>
2019-11-26 20:03:47 +0100

    arduino lib

--- arduino.ino
+++ arduino.ino
@@ -1,9 +1,13 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <DHT.h>
+#include <SPI.h>
+#include <Ethernet.h>
 
+void setup() 
+{
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+
 
 }
