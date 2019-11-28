da24e5b1a10f4ed1d16d851ea2a8e55033bcf7eb
Roland <roland.debnarik@gmail.com>
2019-11-28 20:49:05 +0100

    add vars, loop and function

--- arduino.ino
+++ arduino.ino
@@ -14,8 +14,22 @@ const unsigned long postingInterval = 10*1000;
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
 
+long previousMillis = 0;
+unsigned long currentMillis = 0;
+long interval = 250000; 
 int t = 0;
 int h = 0;
+String data;
+String erzekeles = "";
+String homerseklet = "";
+String paratartalom = "";
+int i = 0;
+String url = "";
+boolean kiolvas = true;
+boolean hall = false;
+String html = "";
+int val;
+int iState;
 
 
 void setup() 
@@ -61,6 +75,88 @@ Serial.println("Initialize Ethernet with DHCP:");
 
 void loop() 
 {
+if(i < 15)
+  {
+    Serial.println("I erteke: " + i);
+    val = digitalRead(30);
+    if(val == HIGH)
+    {
+      i = 49;
+      val = LOW;
+    }
+    if (html.indexOf("nochange") < 0)
+    {
+      if(html.indexOf("hall:on") > 0)
+      {
+        digitalWrite(24, HIGH);
+      }
+      else
+      {
+        digitalWrite(24,LOW);
+      }
+      
+      if(html.indexOf("kitchen:on") > 0)
+      {
+        digitalWrite(26, HIGH);
+      }
+      else
+      {
+        digitalWrite(26,LOW);
+      }
+    
+      if(html.indexOf("bathroom:on") > 0)
+      {
+        digitalWrite(28, HIGH);
+      }
+      else
+      {
+        digitalWrite(28,LOW);
+      }
+    }
+    
+    url ="GET /API/read.php HTTP/1.1";
+    httpRequestHomerseklet();
+    
+    delay(500);
+    i++;
+  }
+  else if (i == 50)
+  {
+    url = "POST /API/add.php?temp1=0&hum1=0&move=1 HTTP/1.1";
+      httpRequestHomerseklet();
+    i = 0;
+  }
+  else
+  {
+    homerseklet = "";
+    paratartalom = "";
+  
+    h = (int) dht.readHumidity();
+    t = (int) dht.readTemperature();
+    
+    homerseklet = String(t);
+    paratartalom = String(h);
+    url = "POST /API/add.php?temp1=" + homerseklet + "&hum1=" + paratartalom + "&move=0 HTTP/1.1";
+      httpRequestHomerseklet();
+    
+    i = 0;
+  }
 
+}
 
+void httpRequestHomerseklet() {
+  client.stop();
+    if (client.connect(server, 80)) {
+    client.println(url);
+    client.println("Host: www.gklb.intm020.debnarik.com");
+    client.println("User-Agent: arduino-ethernet");
+    client.println("Connection: close");
+    client.println();
+    
+
+    lastConnectionTime = millis();
+  } else {
+    Serial.println("connection failed");
+    void(* resetFunc) (void) = 0;
+  }
 }
