b79c7376f80f180defbaf27d256153b16b3a833b
Roland <roland.debnarik@gmail.com>
2019-11-28 18:39:59 +0100

    init vars, dht begin, disable sd card port

--- arduino.ino
+++ arduino.ino
@@ -14,9 +14,22 @@ const unsigned long postingInterval = 10*1000;
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
 
+int t = 0;
+int h = 0;
+
 
 void setup() 
 {
+dht.begin(); 
+h = (int) dht.readHumidity(); 
+t = (int) dht.readTemperature(); 
+String data = "";
+pinMode(4, OUTPUT);
+digitalWrite(4, HIGH);
+pinMode(24, OUTPUT);
+pinMode(26, OUTPUT);
+pinMode(28, OUTPUT);
+pinMode(30, INPUT);
 }
 
 void loop() 
