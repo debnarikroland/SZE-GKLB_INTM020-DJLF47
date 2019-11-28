20b7d14e0307bd59e88115e40ef5df9c061e4e45
Roland <roland.debnarik@gmail.com>
2019-11-28 18:33:20 +0100

    init ethernet

--- arduino.ino
+++ arduino.ino
@@ -1,7 +1,11 @@
 #include <DHT.h>
 #include <SPI.h>
 #include <Ethernet.h>
-
+byte mac[] = {  0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xAB};
+IPAddress ip(192, 168, 137, 200);
+IPAddress myDns(192, 168, 137, 1);
+char server[] = "www.gklb.intm020.debnarik.com";
+EthernetClient client;
 void setup() 
 {
 }
