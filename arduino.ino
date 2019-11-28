25f300fee9df13bf0aff3d294816b0d7d6cac37d
Roland <roland.debnarik@gmail.com>
2019-11-28 18:38:28 +0100

    init DHT11 sensor

--- arduino.ino
+++ arduino.ino
@@ -6,6 +6,15 @@ IPAddress ip(192, 168, 137, 200);
 IPAddress myDns(192, 168, 137, 1);
 char server[] = "www.gklb.intm020.debnarik.com";
 EthernetClient client;
+
+unsigned long lastConnectionTime = 0;
+const unsigned long postingInterval = 10*1000;
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+
 void setup() 
 {
 }
